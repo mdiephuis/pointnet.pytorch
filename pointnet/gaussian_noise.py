b18ca2ba64b46fe2ee9455133184df500db79fb7
margokat <tysyisya@gmail.com>
2019-06-01 12:41:00 +0200

    Fixed noise function.

--- pointnet/gaussian_noise.py
+++ pointnet/gaussian_noise.py
@@ -1,14 +1,14 @@
 import numpy as np
 import torch
 
-def AddGaussianNoise(pointcloud, sigma = 0.001, percent_points = 0.5):
+def AddGaussianNoise(pointcloud, sigma = 0.1, percent_points = 0.2):
     ''' The function adds zero-mean Gaussian Noise with variance = sigma
-    to all the points of a point Cloud'''
+    to random points of a point Cloud, with a given ratior percent_points'''
     N,D = pointcloud.shape
     mu, sigma = 0, sigma  # mean and standard deviation
     noise = np.random.rand(N,D) * sigma #np.random.normal(mu, sigma, (N,D))
 
     #turn off randomly some part of noise, to apply it only to a part of points
     nums = np.random.choice([0, 1], size=(N,1), p=[(1-percent_points), percent_points])
-    noisypointcloud = pointcloud + (nums*noise)
+    noisypointcloud = pointcloud * ((nums*noise)+(np.ones([N,D]))) # multiply the point cloud by noise vector
     return noisypointcloud
