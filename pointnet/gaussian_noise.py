ab2e268d9f80116e1b4c9ee288e4c7fa335bfbd4
Maurits Diephuis <mdiephuis@gmail.com>
2019-06-01 14:22:49 +0200

    fixed the noise

--- pointnet/gaussian_noise.py
+++ pointnet/gaussian_noise.py
@@ -1,14 +1,24 @@
 import numpy as np
-import torch
 
-def AddGaussianNoise(pointcloud, sigma = 0.1, percent_points = 0.2):
+
+def add_gaussian_noise(pointcloud, sigma=0.1, percent_points=0.2):
     ''' The function adds zero-mean Gaussian Noise with variance = sigma
     to random points of a point Cloud, with a given ratior percent_points'''
-    N,D = pointcloud.shape
-    mu, sigma = 0, sigma  # mean and standard deviation
-    noise = np.random.rand(N,D) * sigma #np.random.normal(mu, sigma, (N,D))
+    N, D = pointcloud.shape
+    noise = np.random.randn(N, D) * sigma  # np.random.normal(mu, sigma, (N,D))
+    noise = normalize(noise)
 
-    #turn off randomly some part of noise, to apply it only to a part of points
-    nums = np.random.choice([0, 1], size=(N,1), p=[(1-percent_points), percent_points])
-    noisypointcloud = pointcloud * ((nums*noise)+(np.ones([N,D]))) # multiply the point cloud by noise vector
+    # turn off randomly some part of noise, to apply it only to a part of points
+    # nums = np.random.choice([0, 1], size=(
+    #     N, 1), p=[(1 - percent_points), percent_points])
+    # # multiply the point cloud by noise vector
+    noisypointcloud = pointcloud + noise
+    # clamp [-1, 1]
+
     return noisypointcloud
+
+
+def normalize(x):
+    min_x = np.min(x)
+    max_x = np.max(x)
+    return -1 + (2.0 / (max_x - min_x) * (x - min_x))
